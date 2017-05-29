0a3079c94d78118ab57e71d4bb97d87309ea4e32
Bill Boughton <boughton.william@gmail.com>
2017-05-29 17:25:58 -0600

    trying appdebug true again

--- mongo_connect.py
+++ mongo_connect.py
@@ -92,6 +92,6 @@ def remove(name):
     return 'user removed'
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.debug = True
+    app.run(debug=True)
+    # app.debug = True
     app.run(host='0.0.0.0', port=port)
