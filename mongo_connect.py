c95bef9d64fd99703757766410ab6c5a193afc46
Bill Boughton <boughton.william@gmail.com>
2017-05-29 17:33:21 -0600

    flask and heroku is hard

--- mongo_connect.py
+++ mongo_connect.py
@@ -2,7 +2,8 @@ from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 import json
 import requests
-from pprint import pprint
+from pprint import pprint\
+import os
 # import urllib
 
 app = Flask(__name__)
